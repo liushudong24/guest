ca54fafd1d27ec056349c52f67e9db9f27373492
liushudong24 <664299087@qq.com>
2017-10-29 21:14:04 +0800

    添加session

--- guest/sign/views.py
+++ guest/sign/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 
 
 # Create your views here.
@@ -9,17 +9,24 @@ from django.http import HttpResponse,HttpResponseRedirect
 def index(request):
     return render(request, "index.html")
 
+
 # 登录动作
 def login_action(request):
     if request.method == 'POST':
         username = request.POST.get('username', '')
         password = request.POST.get('password', '')
         if username == 'admin' and password == '123':
-            # return HttpResponse('login success!!!') #1.临时处理
-            return HttpResponseRedirect('/event_manage/')
+            response = HttpResponseRedirect('/event_manage/')
+            # response.set_cookie('user', username, 3600)  # 添加浏览器cookie
+            request.session['user'] = username  # 将session信息记录到浏览器
+            return response
         else:
             # return HttpResponse('login error!!!')
             return render(request, 'index.html', {'error': 'username or password error!'})
+
+
 # 发布会管理
 def event_manage(request):
-    return render(request,"event_manage.html")
+    # username = request.COOKIES.get('user', '')  # 读取浏览器cookies
+    username = request.session.get('user', '')  # 读取浏览器session
+    return render(request, "event_manage.html", {"user": username})
